9a83b8af6732a4b984591e0a9162ff55be328fb4
akash-gupta-146 <ag.akash007@gmail.com>
2018-01-13 18:28:45 +0530

    added modules

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,15 +2,24 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { ThumbnailsComponent } from './thumbnails/thumbnails.component';
+import { ImageDetailsComponent } from './image-details/image-details.component';
+import { RouteModule } from './/router.module';
+import { ImageService } from './image.service';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    ThumbnailsComponent,
+    ImageDetailsComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouteModule
   ],
-  providers: [],
+  providers: [ImageService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
